c2f89797ad3edc08f3bcbed981278d2692520132
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-06-09 20:11:00 +0530

    ejs

--- feature.js
+++
@@ -1,8 +0,0 @@
-const gfName ="Eva Rahman";
-const lovePercent= ()=>{
-    return (`Hello ${gfName}! I love you so much!`);
-}
-const like= lovePercent();
-//export a function
-exports.like = like;
-exports.gfName = gfName;
--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-const http = require('http');
+import express  from "express";
 
-const feature = require('./feature');
+const app = express();
+const port = 3000;
+// Set the view engine to ejs
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+    res.render("index");
+    }
+);
 
-console.log(feature.like);
-console.log(feature.gfName);
-const server= http.createServer((req, res) => {
-    if(req.url === '/'){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(`<h1>Hello World!  </h1> ${feature.gfName} ${feature.like}`);
 
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
     }
-    else if(req.url === '/about'){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>This is about page</h1>');
-    }
-    else if(req.url === '/contact'){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>This is contact page</h1>');
-    }
-    else{
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    res.end('<h1>404 not found</h1>');
-    }
-})
-
-server.listen(5000,()=>{
-    console.log('Server running at http://localhost:5000');
-})
+);
