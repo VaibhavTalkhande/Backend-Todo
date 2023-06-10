200394d076d7c04a51c94a187f12786ef85a2781
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-06-10 21:27:57 +0530

    database connection

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import express  from "express";
-
+import path from "path";
+import mongoose from "mongoose";
 const app = express();
+// Connect to MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.7.1", {
+    dbName:"baackend",
+})
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
+const messageSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+});
+const Message = mongoose.model("Message", messageSchema);
+// Get the default connection
 const port = 3000;
+const users = [];
+// Set the static folder
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true }));
 // Set the view engine to ejs
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
@@ -9,8 +26,36 @@ app.get("/", (req, res) => {
     }
 );
 
+app.get("/success", (req, res) => {
+    res.render("success");
+     
+    }
+);
+
+app.post("/contact", async(req, res) => {
+    const { name, email } = req.body;
+    await Message.create({ name, email });
+
+    
+    res.redirect("/success");
+    }
+
+);
+app.get("/users", (req, res) => {
+    res.json(
+        {
+            users,
+        }
+    );
+    
+    }
+);
+
+
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
     }
 );
+
