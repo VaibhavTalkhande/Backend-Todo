69623489f35f60b33393611e951641a1cf78df51
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-06-12 23:10:13 +0530

    auth done

--- index.js
+++ index.js
@@ -1,88 +1,114 @@
-import express  from "express";
+import express from "express";
 import path from "path";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-const app = express();
-// Connect to MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.7.1", {
-    dbName:"baackend",
-})
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
-const messageSchema = new mongoose.Schema({
-    name: String,
-    email: String,
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017", {
+    dbName: "baackend",
+  })
+  .then(() => console.log("Database Connected"))
+  .catch((e) => console.log(e));
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
 });
-const Message = mongoose.model("Message", messageSchema);
-// Get the default connection
-const port = 3000;
-const users = [];
-// Set the static folder
+
+const User = mongoose.model("User", userSchema);
+
+const app = express();
+
+// Using Middlewares
 app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// Set the view engine to ejs
+
+// Setting up View Engine
 app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-    // const token = req.cookies.token;
-     // destructuring 
-       const { token } = req.cookies;    
-        
-        if (token) {
-            res.render("logout");
-        }
-        else {
-            res.render("login");
-        }
 
+const isAuthenticated = async (req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const decoded = jwt.verify(token, "sdjasdbajsdbjasd");
 
-    }
-);
-app.post("/login", (req, res) => {
-    res.cookie("token", "12345", { httpOnly: true,expires: new Date(Date.now() + 900000)});
-    res.redirect("/");
-    }
-);
+    req.user = await User.findById(decoded._id);
+
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
+
+app.get("/", isAuthenticated, (req, res) => {
+  res.render("logout", { name: req.user.name });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  let user = await User.findOne({ email });
+
+  if (!user) return res.redirect("/register");
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch)
+    return res.render("login", { email, message: "Incorrect Password" });
+
+  const token = jwt.sign({ _id: user._id }, "sdjasdbajsdbjasd");
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 60 * 1000),
+  });
+  res.redirect("/");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  let user = await User.findOne({ email });
+  if (user) {
+    return res.redirect("/login");
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  const token = jwt.sign({ _id: user._id }, "sdjasdbajsdbjasd");
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 60 * 1000),
+  });
+  res.redirect("/");
+});
+
 app.get("/logout", (req, res) => {
+  res.cookie("token", null, {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.redirect("/");
+});
 
-    res.clearCookie("token");
-    res.redirect("/");
-    }
-);
-app.get("/home", (req, res) => {
-    res.render("login");
-    }
-);
-app.get("/success", (req, res) => {
-    res.render("success");
-     
-    }
-);
-
-app.post("/contact", async(req, res) => {
-    const { name, email } = req.body;
-    await Message.create({ name, email });
-
-    
-    res.redirect("/success");
-    }
-
-);
-app.get("/users", (req, res) => {
-    res.json(
-        {
-            users,
-        }
-    );
-    
-    }
-);
-
-
-
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-    }
-);
-
+app.listen(5000, () => {
+  console.log("Server is running on port");
+  console.log("http://localhost:5000");
+});
