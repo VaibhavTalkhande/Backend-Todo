69fd55169bf7545ba0421f63d03cedcfe637cb54
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-06-13 19:17:53 +0530

    auth bcrypt

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ const isAuthenticated = async (req, res, next) => {
     const decoded = jwt.verify(token, "sdjasdbajsdbjasd");
 
     req.user = await User.findById(decoded._id);
-
     next();
   } else {
     res.redirect("/login");
@@ -54,52 +53,42 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("register");
 });
-
-app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  let user = await User.findOne({ email });
-
-  if (!user) return res.redirect("/register");
-
-  const isMatch = await bcrypt.compare(password, user.password);
-
-  if (!isMatch)
-    return res.render("login", { email, message: "Incorrect Password" });
-
-  const token = jwt.sign({ _id: user._id }, "sdjasdbajsdbjasd");
-
-  res.cookie("token", token, {
-    httpOnly: true,
-    expires: new Date(Date.now() + 60 * 1000),
+app.post("/login", async(req,res)=>{
+  const {email,password} = req.body;
+  let user =await User.findOne({email})
+  if(!user){
+    return res.redirect("/register");
+  } 
+  const isMatch =await bcrypt.compare(password,user.password);
+  if(!isMatch){
+    return res.render("login",{email,message:"Incorrect Password"});
+  }
+  const token= jwt.sign({_id:user._id},"sdjasdbajsdbjasd")
+  res.cookie("token",token,{
+    httpOnly:true,
+    expires:new Date(Date.now() + 60 * 1000),
   });
   res.redirect("/");
 });
-
-app.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
-
-  let user = await User.findOne({ email });
-  if (user) {
+app.post("/register",async(req,res)=>{
+  const {name,email,password}=req.body;
+  let user = await User.findOne({email});
+  if(user){
     return res.redirect("/login");
   }
-  const hashedPassword = await bcrypt.hash(password, 10);
-
+  const hashedPassword = await bcrypt.hash(password,10);
   user = await User.create({
     name,
     email,
-    password: hashedPassword,
+    password:hashedPassword,
   });
-
-  const token = jwt.sign({ _id: user._id }, "sdjasdbajsdbjasd");
-
-  res.cookie("token", token, {
-    httpOnly: true,
+  const token = jwt.sign({_id:user._id},"sdjasdbajsdbjasd");
+  res.cookie("token",token,{
+    httpOnly:true,
     expires: new Date(Date.now() + 60 * 1000),
-  });
-  res.redirect("/");
+    });
+    res.redirect("/");
 });
-
 app.get("/logout", (req, res) => {
   res.cookie("token", null, {
     httpOnly: true,
